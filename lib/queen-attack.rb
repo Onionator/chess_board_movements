74ac4ab38a784c9b31cbad74a783c496b16b8bd2
Student <example@example.com>
2019-07-15 16:46:26 -0700

    added conditional evals for other chess pieces

--- lib/queen-attack.rb
+++ lib/queen-attack.rb
@@ -6,7 +6,7 @@ require('pry')
 #   end
 #
 #   def queen_attack(move)
-#     if self.position[0] == move[0] || self.position[1] == move[1]
+#     if self.position[0] == move[0] || self.position[1] == move[1] || (move[0] - self.position[0]).abs == (move[1] - self.position[1]).abs
 #       true
 #     end
 #   end
@@ -18,6 +18,30 @@ require('pry')
 class Array
   def queen_attack?(move)
     if self[0] == move[0] || self[1] == move[1] || (move[0] - self[0]).abs == (move[1] - self[1]).abs
+      true
+    end
+  end
+
+  def rook_attack?(move)
+    if self[0] == move[0] || self[1] == move[1]
+      true
+    end
+  end
+
+  def bishop_attack?(move)
+    if (move[0] - self[0]).abs == (move[1] - self[1]).abs
+      true
+    end
+  end
+
+  def king_attack?(move)
+    if  (move[0] - self[0]).abs == 1 || (self[1] - move[1]).abs == 1
+      true
+    end
+  end
+
+  def knight_attack?(move)
+    if  (move[0] - self[0]).abs == 2 && (self[1] - move[1]).abs == 1 || (move[0] - self[0]).abs == 1 && (self[1] - move[1]).abs == 2
       true
     end
   end
--- spec/queen_attack_spec.rb
+++ spec/queen_attack_spec.rb
@@ -1,7 +1,7 @@
 require('rspec')
 require('queen-attack')
 
-describe('Array#queen_attack?') do
+describe('Array') do
   it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
     expect([1,1].queen_attack?([1, 3])).to(eq(true))
     expect([4,3].queen_attack?([2, 3])).to(eq(true))
@@ -9,5 +9,33 @@ describe('Array#queen_attack?') do
     expect([4,5].queen_attack?([6, 3])).to(eq(true))
     expect([4,5].queen_attack?([2, 7])).to(eq(true))
     expect([4,5].queen_attack?([0, 1])).to(eq(true))
+  end
+
+  it('is false if the coordinates are not horizontally or vertically in line with each other') do
+    expect([1,1].rook_attack?([1, 3])).to(eq(true))
+    expect([4,3].rook_attack?([2, 3])).to(eq(true))
+  end
+
+  it('is false if the coordinates are not horizontally or vertically in line with each other') do
+    expect([4,5].bishop_attack?([6, 7])).to(eq(true))
+    expect([4,5].bishop_attack?([6, 3])).to(eq(true))
+    expect([4,5].bishop_attack?([2, 7])).to(eq(true))
+    expect([4,5].bishop_attack?([0, 1])).to(eq(true))
+  end
+
+  it('is false if the coordinates are not horizontally, vertically, or diagonally in line with each other') do
+    expect([1,1].king_attack?([1, 2])).to(eq(true))
+    expect([4,3].king_attack?([3, 3])).to(eq(true))
+    expect([1,1].king_attack?([2, 2])).to(eq(true))
+    expect([1,1].king_attack?([0, 0])).to(eq(true))
+    expect([1,1].king_attack?([0, 2])).to(eq(true))
+    expect([1,1].king_attack?([2, 0])).to(eq(true))
+  end
+
+  it('is false if the coordinates are not horizontally or vertically in line with each other') do
+    expect([4,0].knight_attack?([5, 2])).to(eq(true))
+    expect([4,0].knight_attack?([3, 2])).to(eq(true))
+    expect([3,3].knight_attack?([5, 2])).to(eq(true))
+    expect([3,3].knight_attack?([1, 2])).to(eq(true))
   end
 end
